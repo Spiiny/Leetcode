cf6ae740a1399ddbc1ff01700340554a1f34473c
Jayanth Pravin R P <jayanthpravinrp@gmail.com>
2026-04-02 23:05:48 +0530

    121 Best Time to Buy and Sell Stock - 0ms 100.00% | 97.22MB 86.49%

---
+++ 0121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int min = prices[0];
+        int res=0;
+        for(int i=1;i<prices.size();i++){
+            if(prices[i]<min) min = prices[i];
+            res = max(res,prices[i]-min);
+        }
+        return res;
+    }
+};
